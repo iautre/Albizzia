79dea4f6bc97a41ab20fcbddc526a0c3afbc804e
a little <mo@autre.cn>
2021-01-24 21:36:30 +0800

    d

--- pages/_a/_slug.vue
+++ pages/_a/_slug.vue
@@ -1,24 +1,55 @@
 <template>
   <div>
-      <h1>{{postData.title}}</h1>
-      <div class="meta">
-        <span><a-icon type="calendar" style="margin-right: 2px" />
-            {{postData.modified | dataFormat("yyyy-MM-dd")}}</span>
-        <span><a-icon type="tags" style="margin-right: 2px;margin-left: 8px;" />
-            {{postData.categories}}
-            <template v-for="tag in postData.tags">
-                {{ tag }}
-            </template>
-        </span>
-        <span ><a-icon @click="like" :theme="iconTheme" type="star" style="margin-right: 2px;margin-left: 8px;" />
-            {{postData.likes}}
-        </span>
-        <span ><a-icon type="message" style="margin-right: 2px;margin-left: 8px;" />
-            {{postData.comments}}
-        </span>
-      </div>
-      <div class="text" v-html="$md.render(postData.content)"></div>
+    <h1>{{postData.title}}</h1>
+    <div class="meta">
+      <span><a-icon type="calendar" style="margin-right: 2px" />
+          {{postData.modified | dataFormat("yyyy-MM-dd")}}</span>
+      <span><a-icon type="tags" style="margin-right: 2px;margin-left: 8px;" />
+          {{postData.categories}}
+          <template v-for="tag in postData.tags">
+              {{ tag }}
+          </template>
+      </span>
+      <span ><a-icon @click="like" :theme="iconTheme" type="star" style="margin-right: 2px;margin-left: 8px;" />
+          {{postData.likes}}
+      </span>
+      <span ><a-icon type="message" style="margin-right: 2px;margin-left: 8px;" />
+          {{postData.comments}}
+      </span>
     </div>
+    <div class="text" v-html="$md.render(postData.content)"></div>
+    <a-row type="flex">
+      <a-col flex="auto"></a-col>
+      <a-col><a-button icon="star" @click="like">{{postData.likes}}</a-button></a-col>
+      <!--
+      <a-col flex="10px"/>
+      <a-col>
+        <a-popover title="Title">
+          <template slot="content">
+            <p>Content</p>
+            <p>Content</p>
+          </template>
+          <a-button  icon="qrcode">
+            推送到手机
+          </a-button>
+        </a-popover>
+      </a-col>
+      <a-col flex="10px"/>
+      <a-col>
+        <a-popover title="Title">
+          <template slot="content">
+            <p>Content</p>
+            <p>Content</p>
+          </template>
+          <a-button  icon="wechat">
+            推送到微信
+          </a-button>
+        </a-popover>
+      </a-col>
+      -->
+      <a-col flex="auto"></a-col>
+    </a-row>
+  </div>
 </template>
 <script>
 export default {

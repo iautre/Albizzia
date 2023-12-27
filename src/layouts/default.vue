f3e336f5e1f513380113321edc31acf7bd0abb99
a little <little@autre.cn>
2023-12-27 21:22:33 +0800

    调整样式

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -57,9 +57,6 @@ const {diary} = defineProps({
 const mainContainerRef = ref<HTMLElement | undefined>(undefined)
 </script>
 <style scoped>
-.n-back-top{
-  z-index: 999;
-}
 @media screen and (min-width: 1024px){
   .top-header {
     display: none;

7ed19a507d214e224438b0b6baaae439d891f051
a little <little@autre.cn>
2024-04-07 13:18:06 +0800

    调整head

--- src/components/ContentHeader.vue
+++ src/components/ContentHeader.vue
@@ -1,21 +1,20 @@
 <template>
   <n-space>
-    <n-button text size="small">
+    <n-button :bordered="false" size="small">
       <DateTimeFormat :timeUnixStr="timeUnixStr"/>
     </n-button>
-    <n-button text size="small" v-for="(category, index) in categories" :key="category.slug">
-      <router-link :to="'/topic/'+category.slug">{{ category.name}}</router-link>
-    </n-button>
-    <n-button text size="small"  v-for="(tag, index) in tags" :key="tag">
-        {{ index !== 0 ? ' ' + tag : tag }}
-    </n-button>
-    <n-button text size="small" v-if="gps">
-<!--      <template #icon>-->
-<!--        <n-icon :component="LocationOutline"></n-icon>-->
-<!--      </template>-->
+    <n-button :bordered="false" size="small" v-if="gps">
+      <!--      <template #icon>-->
+      <!--        <n-icon :component="LocationOutline"></n-icon>-->
+      <!--      </template>-->
       {{gps}}
     </n-button>
-
+    <n-button round size="small" v-for="(category, index) in categories" :key="category.slug">
+      <router-link :to="'/topic/'+category.slug">{{ category.name}}</router-link>
+    </n-button>
+    <n-button round dashed size="small"  v-for="(tag, index) in tags" :key="tag">
+        {{ index !== 0 ? ' ' + tag : tag }}
+    </n-button>
   </n-space>
 </template>
 
